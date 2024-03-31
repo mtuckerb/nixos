c2cc2068218fba0a3fb911f8a89496171fa57b45
Tucker Bradford <tucker@tuckebradford.com>
2024-03-30 21:14:56 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS Gui Developer";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, home-manager, ...}: 
+
+    let lib = nixpkgs.lib; 
+
+  in {
+
+    programs._1password = { enable = true; };
+    programs._1password-gui = { enable = true; };
+    polkitPolicyOwners = [ "mtuckerb" ];
+
+    nixosConfigurations = {
+      tuckernix = lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./hardware-configuration.nix 
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+# home-manager.users.mtuckerb = import /home/mtuckerb/home.nix;
+            }
+        ];			
+      };
+    };
+  };
+}
